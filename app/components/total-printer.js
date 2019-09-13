44c0d67ebf22e34a74c3ab3ac19f31749dc6c2a3
nicholasborghese <nicholas.borghese@gmail.com>
2019-09-13 16:18:53 -0400

    adding details

---
+++ app/components/total-printer.js
@@ -0,0 +1,8 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+    actions: {
+        document.getElementById("totalTip").style.display = "none";
+        document.getElementById("totalWithTip").style.display = "none";   
+    }
+});
